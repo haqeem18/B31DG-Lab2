90d2f3f5a64f52023ac62de97f20b3cd533949ed
haqeem18 <67266213+haqeem18@users.noreply.github.com>
2023-03-09 20:39:19 +0000

    Adding Task 4
    Adding Task 4 to calculate the last 4 readings of a 3.3V analog signal using analogRead and measuring the time taken to perform Task 2, 3, and 4.

--- B31DG-Lab2.ino
+++ B31DG-Lab2.ino
@@ -7,27 +7,35 @@ Ticker Cycle;
 #define LED_1_Output 4
 #define Signal_2_Input 1
 #define Signal_3_Input 2
+#define Analog_4_Input 3
 
 // Instantiate variables
 unsigned long halflambda;
 unsigned int freq;
+
+int ADC;
+float volts [4];
+int v=0;
+float avg;
 
 void setup() 
 {
   // Serial monitor
   Serial.begin(9600);
 
-  // Set output pin
+  // Setup GPIOs
   pinMode(LED_1_Output, OUTPUT);
   pinMode(Signal_2_Input, INPUT);
+  pinMode(Signal_3_Input, INPUT);
+  pinMode(Analog_4_Input, INPUT);
 
   // Set ticker interval 
-  Cycle.attach_ms(2000, Task3);
+  // Cycle.attach_ms(2000, Task3);
+  Cycle.attach_ms(2000, Task4);
 }
 
 void loop() 
 {
-  
 }
 
 // Task 1 execution
@@ -47,6 +55,9 @@ void Task1()
 
 void Task2()
 {
+  // Measure Task start time
+  int starttime = micros();
+
   // Measure the HIGH period of the square wave
   halflambda = pulseIn(Signal_2_Input, HIGH);
   
@@ -56,12 +67,22 @@ void Task2()
   // Print frequency of signal within 333Hz and 1000Hz
   if ((freq >= 333) && (freq <= 1000))
   {
+    Serial.print("Frequency is ");
     Serial.println(freq);
   }
+
+  // Measure Task end time
+  int endtime = micros();
+  int time = endtime - starttime;
+  Serial.print("Task duration is ");
+  Serial.println(time);
 }
 
 void Task3()
 {
+  // Measure Task start time
+  int starttime = micros();
+
   // Measure the HIGH period of the square wave
   halflambda = pulseIn(Signal_3_Input, HIGH);
   
@@ -71,6 +92,46 @@ void Task3()
   // Print frequency of signal within 500Hz and 1000Hz
   if ((freq >= 500) && (freq <= 1000))
   {
+    Serial.print("Frequency is ");
     Serial.println(freq);
   }
+
+  // Measure Task end time
+  int endtime = micros();
+  int time = endtime - starttime;
+  Serial.print("Task duration is ");
+  Serial.println(time);
+}
+
+void Task4()
+{
+  // Measure Task start time
+  int starttime = micros();
+
+  // Point array back to start
+  if (v == 4)
+  {
+    v = 0;
+  }
+
+  // Take ADC value of analogue signal
+  volts[v] = analogRead(Analog_4_Input) * 0.000805664;
+  v++;
+
+  // Sum the last 4 readings
+  for (int i = 0; i < 4; i++)
+  {
+    avg += volts[i]; 
+  }
+
+  // Calculating the average
+  avg = avg / 4;
+  Serial.print("Average is ");
+  Serial.println(avg);
+
+  // Measure Task end time
+  int endtime = micros();
+  int time = endtime - starttime;
+  Serial.print("Task duration is ");
+  Serial.println(time);
 }
